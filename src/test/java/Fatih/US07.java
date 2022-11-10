dc38c04f67bdbaff506186e981426eaf4ead2a49
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2022-11-10 13:02:36 +0100

    US08 10.11 (1) Fatih

---
+++ src/test/java/Fatih/US07.java
@@ -0,0 +1,9 @@
+package Fatih;
+
+public class US07 {
+
+    public static void main(String[] args) {
+        System.out.println("Fatih, kendi branch'inde calismaya basladi...");
+    }
+
+}
---
+++ src/test/java/Fatih/US08.java
@@ -0,0 +1,9 @@
+package Fatih;
+
+public class US08 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Fatih, kendi branch'inde calismaya devam ediyor");
+    }
+}
